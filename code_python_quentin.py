a3fa643bbc4aad4b490e7ea74a29e111df1924ae
Etienne VOILLIOT <etiennevoilliot@homair.com>
2022-02-01 15:15:15 +0100

    Formattage du code simplement

--- code_python_quentin.py
+++ code_python_quentin.py
@@ -11,38 +11,41 @@ pixel_fond = im.load()
 
 # terrain de jeu
 
-surface = pygame.display.set_mode((700,710))
+surface = pygame.display.set_mode((700, 710))
 pygame.display.set_caption("Ghosts 'n Goblins")
 
 # acteurs graphiques
 
 hero_gauche = pygame.image.load('hero2.png')
 hero_droite = pygame.image.load('hero3.png')
-fond=pygame.image.load('fond_GNG.bmp')
-super=pygame.image.load('super.png')
-echec=pygame.image.load('echec.png')
-mechant1=pygame.image.load('dragon1.png')
-mechant2=pygame.image.load('dragon2.png')
-zombie1=pygame.image.load('zombie1.png')
-zombie2=pygame.image.load('zombie2.png')
+fond = pygame.image.load('fond_GNG.bmp')
+super = pygame.image.load('super.png')
+echec = pygame.image.load('echec.png')
+mechant1 = pygame.image.load('dragon1.png')
+mechant2 = pygame.image.load('dragon2.png')
+zombie1 = pygame.image.load('zombie1.png')
+zombie2 = pygame.image.load('zombie2.png')
 
 # sous fonctions
 
+
 def victoire():
-    surface.blit(super,(350,355))
+    surface.blit(super, (350, 355))
     pygame.display.update()
 
+
 def defaite():
-    surface.blit(echec,(350,355))
+    surface.blit(echec, (350, 355))
     pygame.display.update()
 
 # fonction principale
 
+
 def principal():
 
-    flag=False
-    n=0
-    run=True
+    flag = False
+    n = 0
+    run = True
 
     isjump = False
     v = 5
@@ -50,101 +53,101 @@ def principal():
 
     sens = 'right'
 
-    x,y=10,45
-    x_mvt,y_mvt=0,0
-    xm,ym=350,300
-    xm_mvt,ym_mvt=1,0
+    x, y = 10, 45
+    x_mvt, y_mvt = 0, 0
+    xm, ym = 350, 300
+    xm_mvt, ym_mvt = 1, 0
 
     while run:
 
-        time.sleep(0.01)                    # temps de repos/rafraichissement de 10ms
-        n+=1                                # pour que le mechant change de tete et deplacement au bout de 30 x 10 = 300ms
+        # temps de repos/rafraichissement de 10ms
+        time.sleep(0.01)
+        # pour que le mechant change de tete et deplacement au bout de 30 x 10 = 300ms
+        n += 1
         for event in pygame.event.get():
-            if event.type==pygame.QUIT:
-                run=False
-            if event.type==pygame.KEYDOWN:
-                if event.key==pygame.K_LEFT:
-                    x_mvt=-1
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_mvt = -1
                     sens = 'left'
-                if event.key==pygame.K_RIGHT:
-                    x_mvt=1
+                if event.key == pygame.K_RIGHT:
+                    x_mvt = 1
                     sens = 'right'
-                if event.key==pygame.K_UP:
+                if event.key == pygame.K_UP:
                     if isjump == False:
-                            isjump = True
-                    if isjump :
-                        F =(1 / 2)*m*(v**2)
-                        y-= F
+                        isjump = True
+                    if isjump:
+                        F = (1 / 2)*m*(v**2)
+                        y -= F
                         v = v-1
-                        if v<0:
-                            m =-1
-                        if v ==-6:
+                        if v < 0:
+                            m = -1
+                        if v == -6:
                             isjump = False
                             v = 5
                             m = 1
 
                     pygame.time.delay(10)
 
-            if event.type==pygame.KEYUP:
-                x_mvt=0
+            if event.type == pygame.KEYUP:
+                x_mvt = 0
 
+        x += x_mvt        # deplacement hero
 
-
-        x+=x_mvt        # deplacement hero
-
-
-
-        xm+=xm_mvt      # deplacement mechant
+        xm += xm_mvt      # deplacement mechant
 
         # correction du deplacement si besoin
 
-        if (xm<10) or (xm>260):      # le mechant reste dans le terrain mais traverse les murs !!!
-            xm-=xm_mvt
+        if (xm < 10) or (xm > 260):      # le mechant reste dans le terrain mais traverse les murs !!!
+            xm -= xm_mvt
 
-        if (pixel_fond[x,y+65]==(153,153,153)):
+        if (pixel_fond[x, y+65] == (153, 153, 153)):
             defaite()
             time.sleep(2)
-            x,y=10,45
-            xm,ym=350,300
-            xm_mvt,ym_mvt=1,0
+            x, y = 10, 45
+            xm, ym = 350, 300
+            xm_mvt, ym_mvt = 1, 0
 
-        if (pixel_fond[x+65,y]==(85,128,124)):                 # toucher  pixel= porte = victoire
+        # toucher  pixel= porte = victoire
+        if (pixel_fond[x+65, y] == (85, 128, 124)):
             victoire()
             time.sleep(2)
-            x,y=10,45
-            xm,ym=350,300
-            xm_mvt,ym_mvt=1,0
+            x, y = 10, 45
+            xm, ym = 350, 300
+            xm_mvt, ym_mvt = 1, 0
 
-        if (pixel_fond[x,y+65]==(118,139,80)):
-            y_mvt=0
+        if (pixel_fond[x, y+65] == (118, 139, 80)):
+            y_mvt = 0
         else:
-            y_mvt=1
+            y_mvt = 1
 
-        if (abs(x-xm)<20) and (abs(y-ym)<15):                   # si collision mechant = defaite
+        if (abs(x-xm) < 20) and (abs(y-ym) < 15):                   # si collision mechant = defaite
             defaite()
             time.sleep(2)
-            x,y=10,10
-            xm,ym=350,300
-            xm_mvt,ym_mvt=1,0
+            x, y = 10, 10
+            xm, ym = 350, 300
+            xm_mvt, ym_mvt = 1, 0
 
         # affichages des acteurs graphiques
 
-        surface.blit(fond,(0,0))
+        surface.blit(fond, (0, 0))
 
         # Image héro, droite ou gauche.
         if (sens == 'left'):
-          surface.blit(hero_droite, (x, y))
+            surface.blit(hero_droite, (x, y))
         else:
-          surface.blit(hero_gauche, (x, y))
+            surface.blit(hero_gauche, (x, y))
 
-        if n>30:                                # 30x10ms=300ms histoire de faire alternner mechant1 et mechant2
-            n=0
-            flag=not(flag)                          # inversion du flag= True/False toutes les 300ms
+        if n > 30:                                # 30x10ms=300ms histoire de faire alternner mechant1 et mechant2
+            n = 0
+            # inversion du flag= True/False toutes les 300ms
+            flag = not(flag)
 
         if(flag):                               # faire alternner mechant1 et mechant2
-            surface.blit(mechant1,(xm,ym))
+            surface.blit(mechant1, (xm, ym))
         else:
-            surface.blit(mechant2,(xm,ym))
+            surface.blit(mechant2, (xm, ym))
 
         pygame.display.update()                 # rafraichissement
 
